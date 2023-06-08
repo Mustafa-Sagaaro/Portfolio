d053449ab692127100ae5a4e8c43bdd1cb35012d
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-06-08 15:16:09 +0200

    Navbar bearbeitet und resposive gemacht

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes } from "react-icons/fa";
+import "../styles/main.css"
+import { useRef } from 'react';
+
 
 const Navbar = () => {
+    
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+        
+    }
+    
     return (
-        <div>Navbar</div>
+        <header>
+            <h3>Logo</h3>
+            <nav ref={navRef}>
+                <a href="/#">About</a>
+                <a href="/#">my Work</a>
+                <a href="/#">Contact</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+            
+        </header>
     )
 }
 
