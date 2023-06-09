141b8af67e0e1130ad76cc5018128daf2a30808c
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-06-09 08:34:42 +0200

    Navbar fixed and added Animations

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div>About</div>
-    )
-}
+  return (
+    <section id="About" class="hidden">
+      <div id="description">
+        <h2>Immerse yourself in the fascinating world of technology with me!</h2>
+        <p>
+        At just
+        16 years old, I'm in my first year of apprenticeship as an application
+        developer. I have a strong passion for creating innovative solutions and
+        pushing the boundaries of programming. With my commitment to excellence
+        and enthusiasm for collaboration, I aim to shape the future of
+        technology.
+        </p>
+        <strong><p>Let's embark on this exciting journey together and create
+        something extraordinary!</p></strong>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "../styles/main.css";
 
 const Hero = () => {
-    return (
-        <div>Hero</div>
-    )
-}
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
 
-export default Hero
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    return () => {
+      hiddenElements.forEach((el) => observer.unobserve(el));
+    };
+  }, []);
+
+  return (
+    <section id="Hero" className="hidden">
+      <div id="introduction">
+        <h2>welcome</h2>
+        <h1>I'm Mustafa Sagaaro</h1>
+        <p>based in Zurich, Switzerland.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
+
+
